1f7f960ec5ac2723b6af35caf3c997461cf7f9bd
davidpattaguanAGPH <105701295+davidpattaguanAGPH@users.noreply.github.com>
2023-08-24 22:48:13 +0800

    staging test

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -43,7 +43,7 @@ export default async function Home() {
       <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32">
         <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
           <h1 className="font-semibold text-3xl sm:text-5xl md:text-6xl lg:text-7xl">
-            Next.js Simple Boilerplate
+            Next.js Simple Boilerplate (Staging)
           </h1>
           <p className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
             A Basic Starter Template for Next.js 13
